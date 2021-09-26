d796b19bece424ac4f7551897fe66ff69bac966d
k-oketa <cist.m220.k.oketa@gmail.com>
2021-09-26 19:25:58 +0900

    プログラムをリファクタリング

--- timetable_miner/timetable_miner/lib.py
+++ timetable_miner/timetable_miner/lib.py
@@ -8,18 +8,22 @@ from timetable_miner import config
 class PdfMiner:
 
     def __init__(self, path_or_buf: Union[str, bytes]):
-        if type(path_or_buf) is bytes:
-            buf = path_or_buf
-            with open('tmp.pdf', 'wb') as f:
-                f.write(buf)
-            self.__dfs = tabula.read_pdf('tmp.pdf', lattice=True, pages='all')
-        else:
-            path = path_or_buf
-            self.__dfs = tabula.read_pdf(path, lattice=True, pages='all')
+        path = self.__preprocess(path_or_buf)
+        self.__dfs = tabula.read_pdf(path, lattice=True, pages='all')
         self.__outward_df = self.__dfs[0][config.outward_columns]
         self.__return_df = self.__dfs[1][config.return_columns]
         self.__outwards = self.__outward_df.to_dict(orient='records')
         self.__returns = self.__return_df.to_dict(orient='records')
+
+    def __preprocess(self, path_or_buf: Union[str, bytes]) -> str:
+        if type(path_or_buf) is bytes:
+            buf = path_or_buf
+            with open('tmp.pdf', 'wb') as f:
+                f.write(buf)
+            return f.name
+        else:
+            path = path_or_buf
+            return path
 
     def outwards_and_returns(self) -> dict:
         return {'往路': self.__outwards, '復路': self.__returns}
@@ -31,4 +35,3 @@ class PdfMiner:
     @property
     def returns(self) -> dict:
         return self.__returns
-
