926367692074531d0a272f98ed661826da62a7a8
k-oketa <cist.m220.k.oketa@gmail.com>
2021-09-27 22:25:55 +0900

    ヘッダにAppBarを追加

--- bus-timetable/src/App.js
+++ bus-timetable/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Container from "@mui/material/Container";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <AppBar>
+                    <Toolbar>
+                        <Typography variant="h6" component="div">
+                            公立千歳科学技術大学 非公式バス時刻表
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+            </header>
+            <Container maxWidth="lg">
+                
+            </Container>
+        </div>
+    );
 }
 
 export default App;
