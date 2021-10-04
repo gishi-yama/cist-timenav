9aa54eb831926e8bad30379bedcd08295c4067d3
k-oketa <cist.m220.k.oketa@gmail.com>
2021-10-04 16:31:51 +0900

    ToolBarのテキストをBodyと同じ並びになるように修正

--- bus-timetable/src/components/Header.js
+++ bus-timetable/src/components/Header.js
@@ -1,15 +1,17 @@
-import {AppBar} from "@mui/material";
+import {AppBar, Container} from "@mui/material";
 import {Toolbar} from "@mui/material";
 import {Typography} from "@mui/material";
 
 function Header() {
     return (
         <AppBar position="static">
-            <Toolbar>
-                <Typography variant="h6" component="div">
-                    公立千歳科学技術大学 非公式バス時刻表
-                </Typography>
-            </Toolbar>
+            <Container maxWidth="xl">
+                <Toolbar>
+                    <Typography variant="h6" component="div">
+                        公立千歳科学技術大学 非公式バス時刻表
+                    </Typography>
+                </Toolbar>
+            </Container>
         </AppBar>
     );
 }
