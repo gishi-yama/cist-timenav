7df245ce1ed78ad7496a9cae6d326f6f0151049b
k-oketa <cist.m220.k.oketa@gmail.com>
2021-10-26 09:14:37 +0900

    ordinalによってpdfを取得する機能を追加

--- pdf_retriever/api.py
+++ pdf_retriever/api.py
@@ -16,9 +16,19 @@ def oldest_url():
     return make_response(jsonify({'url': retriever.retrieve_url(0)}))
 
 
+@app.route('/url/<number>')
+def url_by(number: int):
+    return make_response(jsonify({'url': retriever.retrieve_url(number)}))
+
+
 @app.route('/bytes/oldest')
 def oldest_bytes():
     return app.response_class(retriever.retrieve_bytes(0), mimetype='text/pdf')
+
+
+@app.route('/bytes/<number>')
+def bytes_by(number: int):
+    return app.response_class(retriever.retrieve_bytes(number), mimetype='text/pdf')
 
 
 if __name__ == '__main__':
