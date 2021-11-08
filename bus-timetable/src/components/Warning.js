43db4d8ea52588ba35b44db83f3425307108012c
k-oketa <cist.m220.k.oketa@gmail.com>
2021-11-08 20:49:07 +0900

    サーバのcronの設定に合わせて注意文を変更

--- bus-timetable/src/components/Warning.js
+++ bus-timetable/src/components/Warning.js
@@ -5,7 +5,7 @@ function Warning() {
     return (
         <Alert severity="warning">
             <AlertTitle>注意</AlertTitle>
-            本システムは、1時間ごとの<a href="https://www.chitose.ac.jp/info/access">大学へのアクセス</a>の一番上のPDFの内容を表示しています。PDFの更新状況によっては本日の時刻表を表示しません。特に週明けの朝は更新されない傾向にあります。ご注意ください。
+            本システムは、AM/PM0:00の<a href="https://www.chitose.ac.jp/info/access">大学へのアクセス</a>の一番上のPDFの内容を表示しています。PDFの更新状況によっては本日の時刻表を表示しません。特に週明けの朝は更新されない傾向にあります。ご注意ください。
         </Alert>
     );
 }
